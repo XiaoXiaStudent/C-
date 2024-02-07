d8f5c634abe4203db63f3a58a73a42f5143efc2e
youzi <youzi@outlook.com>
2024-02-07 16:50:02 +0800

    练习

--- main.cpp
+++ main.cpp
@@ -110,16 +110,56 @@ public:
     }
 };
 
-int main() {
-    std::unique_ptr<Entity> entity = std::make_unique<Entity>();
-    std::shared_ptr<Entity> e = std::make_shared<Entity>();
-    std::weak_ptr<Entity> e2 = e;
-    std::cout << e.use_count() << std::endl;
-    {
-        std::shared_ptr<Entity> sharedEntity = e;
-        std::cout << e.use_count() << std::endl;
+
+class String {
+
+private:
+    char *m_Buffer;
+    unsigned int m_Size;
+public:
+
+
+    String(const char *string) {
+        m_Size = strlen(string);
+        m_Buffer = new char[m_Size + 1];
+        memcpy(m_Buffer, string, m_Size);
+        m_Buffer[m_Size] = 0;
     }
-    std::cout << e.use_count() << std::endl;
-    std::cin.get();
-    return 0;
+
+    String(const String &other) : m_Size(other.m_Size) {
+        std::cout << "copy start" << std::endl;
+        m_Buffer = new char[m_Size + 1];
+        memcpy(m_Buffer, other.m_Buffer, m_Size + 1);
+    }
+
+    ~String() {
+        delete[] m_Buffer;
+    }
+
+    char &operator[](unsigned int index) {
+        return m_Buffer[index];
+    }
+
+    friend std::ostream &operator<<(std::ostream &stream, const String &string);
+};
+
+std::ostream &operator<<(std::ostream &stream, const String &string) {
+    stream << string.m_Buffer;
+    return stream;
+}
+
+void PrintString(const String& string) {
+    String copy = string;
+    std::cout << string << std::endl;
+}
+
+
+int main() {
+    String string = "nihaoya";
+    String secend = string;
+
+    secend[2] = 'a';
+
+    PrintString(string);
+    PrintString(secend);
 }
